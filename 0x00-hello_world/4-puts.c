2783299d21ccbd40d50a833fa4e42d4e025b99a2
Mohamed Gamal <mohammed.gamal@live.com>
2023-03-16 16:57:04 -0700

    c puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 /**
  * main - this function prints a comment
- *
- *  
+ * Return: 0 no matter.  
  */
 int main(void)
 {
-/* Print our a comment with escaped quotes */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
