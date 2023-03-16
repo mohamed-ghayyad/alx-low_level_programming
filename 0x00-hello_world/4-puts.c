14174318d08390acdc38415a2c0e88c9aaef3f77
Mohamed Gamal <mohammed.gamal@live.com>
2023-03-16 16:47:07 -0700

    c puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
-
 int main(void)
 {
-
 /**
  *
  * Print our a comment with escaped quotes
  *
  */
-
 puts("\"Programming is like building a multilingual puzzle");
 return 0;
 }
