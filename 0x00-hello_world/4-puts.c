c3c552ff76091a37f6c978ef95e6cec93b4b621e
Mohamed Gamal <mohammed.gamal@live.com>
2023-03-16 16:44:05 -0700

    c puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
+/**
+ *
+ *
+ * Prints exactly "Programming is like building a multilingual puzzle
+ *
+ *
+ */
 int main(void)
 {	
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
