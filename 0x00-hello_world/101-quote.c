4e89b586fb7ad2b0b361a3339bfe5d567db5ba4f
Mohamed Gamal <mohammed.gamal@live.com>
2023-03-16 17:44:51 -0700

    print to the stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - print a message to standard error
+ * Return: 0 always.
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (0);
+}
