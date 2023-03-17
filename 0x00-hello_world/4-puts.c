5dca5fd0b3e58b2a27d435ad4615bfcec9b75704
Mohamed Gamal <mohammed.gamal@live.com>
2023-03-16 17:03:49 -0700

    Put C String

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main - this function prints a comment
- * Return: 0 no matter.  
- */
+* main - this function prints a comment
+* Return: 0 no matter.  
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
