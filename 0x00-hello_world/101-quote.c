c0fef183989ba2d7735ff73b532dd041ebc988df
Mohamed Gamal <mohammed.gamal@live.com>
2023-03-16 18:10:33 -0700

    print to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 30, 1, stderr);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
