6d5c48e9c00ab23ff3a7949f27da6fd72a3d7890
Mohamed Gamal <mohammed.gamal@live.com>
2023-03-16 17:49:39 -0700

    print to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 30);
+fprintf(stderr,"%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (0);
 }
