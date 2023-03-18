2be6dae8421c937b718fde7a9514b4a900635993
Mohamed Gamal <mohammed.gamal@live.com>
2023-03-17 19:13:08 -0700

    alphabet in reverse.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - alphabet game in reverse.
+ * Return: 0 always.
+ **/
+int main(void)
+{
+char alpha;
+for (alpha = 'z'; alpha <= 'a'; alpha--)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
