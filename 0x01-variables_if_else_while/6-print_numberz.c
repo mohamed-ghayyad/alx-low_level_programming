f6293d7ecd5c9b3e89a9ae7104da217404a214b0
Mohamed Gamal <mohammed.gamal@live.com>
2023-03-17 19:03:00 -0700

    print numberz with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - print numbers 0-9
+ * Return: 0 always
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
