a2efb60504fe46c00414020fc863045697ba7b68
Mohamed Gamal <mohammed.gamal@live.com>
2023-03-22 14:31:09 -0700

    put_ char C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include "main.h"
 /**
  * main - print _putchar
  * Return: 0 always.
  */
 int main(void)
 {
-printf("_putchar\n");
+_putchar("_putchar\n");
 return (0);
 }
