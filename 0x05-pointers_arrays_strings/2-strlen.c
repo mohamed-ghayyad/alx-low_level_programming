02182d47e0f9e28ccbae9f899f297ff100a896f4
Mohamed Gamal <mohammed.gamal@live.com>
2023-03-27 17:53:08 -0700

    length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * strlen -  a function that returns the length of a string
+ * _strlen -  a function that returns the length of a string
  * @s: An input string
  * Return: Nothing
  */
